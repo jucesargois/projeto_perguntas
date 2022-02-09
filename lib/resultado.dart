ca7bafaf50ee504ecd279cb09399f7d252b8ab59
Julio <jucesargois@gmail.com>
2022-02-09 02:04:33 -0300

    Inserido get fraseResultado para returno de uma determinada frase com a condição dos valores. Inserido widget para fraseResultado e inserido botão reiniciar com onPressed para a função quandoReiniciarQuestionario.

--- lib/resultado.dart
+++ lib/resultado.dart
@@ -1,16 +1,43 @@
 import 'package:flutter/material.dart';
 
 class Resultado extends StatelessWidget {
-  String texto;
-  Resultado(this.texto, {Key? key}) : super(key: key);
+  final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
+
+  Resultado(this.pontuacao, this.quandoReiniciarQuestionario, {Key? key})
+      : super(key: key);
+
+  String get fraseResultado {
+    if (pontuacao < 8) {
+      return 'Parabéns';
+    } else if (pontuacao < 12) {
+      return 'Você é bom';
+    } else if (pontuacao < 16) {
+      return 'Impressionante';
+    } else {
+      return 'Nível Jedi!';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  Center(
-      child: Text(
-        texto,
-        style: TextStyle(fontSize: 28),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(
+            fraseResultado,
+            style: TextStyle(fontSize: 28),
+          ),
+        ),
+        TextButton(
+          onPressed: quandoReiniciarQuestionario,
+          child: const Text(
+            'Reiniciar?',
+            style: TextStyle(color: Colors.blue, fontSize: 18),
+          ),
+        )
+      ],
     );
   }
 }
